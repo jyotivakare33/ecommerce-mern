a504da42daf9afe3f5b66c6b78d6d15e208f548f
jyoti vakare <jyotivakare33@gmail.com>
2021-04-17 14:51:21 +0530

    payment page changes

--- src/components/PaymentInfo.js
+++ src/components/PaymentInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { initiatePayment }  from './Payment';
+import { Accordion, Card, Breadcrumb } from 'react-bootstrap';
 
 class PaymentInfo extends Component {
 
@@ -46,19 +47,38 @@ class PaymentInfo extends Component {
     render() {
         const { totalAmount } = this.state.cart || 4000;
         return (
-            <div className="content main-login-div">
-                <div className="paymentContainer">
-            <p>Product Price : {totalAmount} </p>
-            <p>Discount : Rs.450</p>
-            <p>Delivery Charges : Rs.45</p>
-            <p>Tax Charges : Rs.250</p>
-            <p>Total Charges : {totalAmount + 450 + 250 + 45}</p>
-            <button className="checkout-add add_address" onClick={this.isLogged}>
-                Pay {totalAmount + 450 + 250 + 45}
-            </button>
-        </div>
-        </div>
-    );
+            <div>
+            <Breadcrumb>
+                    <Breadcrumb.Item href="/checkout">Home</Breadcrumb.Item>
+                    <Breadcrumb.Item href="/address">
+                        {' '}
+                        Add Address
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item href="/payment" active>Payment</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="content">
+                    <Accordion defaultActiveKey="0" className="accordion">
+                        <Card>
+                            <Accordion.Toggle as={Card.Header} eventKey="0">
+                                Payment
+                            </Accordion.Toggle>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>
+                                    <p>Product Price : {totalAmount} </p>
+                                    <p>Discount : Rs.450</p>
+                                    <p>Delivery Charges : Rs.45</p>
+                                    <p>Tax Charges : Rs.250</p>
+                                    <p>Total Charges : {totalAmount + 450 + 250 + 45}</p>
+                                    <button className="checkout-add add_address" onClick={this.isLogged}>
+                                        Pay {totalAmount + 450 + 250 + 45}
+                                    </button>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        </Accordion>
+                        </div> 
+                </div>
+        );
     }
 }
 
